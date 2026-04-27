bbccd1ae79026183b7dcf4cd78972ffed4f6af1d
MauroBarros <barros.mauro@gmail.com>
2026-04-27 17:40:46 +0100

    test: add H2 profile for CI builds

--- src/test/java/com/example/backup_api/BackupApiApplicationTests.java
+++ src/test/java/com/example/backup_api/BackupApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.backup_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackupApiApplicationTests {
 
 	@Test
